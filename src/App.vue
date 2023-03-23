f238a704fbbe6b8677419f9ba9eda01a56d8d267
Moa Englander <moaenglander@gmail.com>
2023-03-23 19:16:48 +0100

    Add components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <script>
-  import CitiesList from './components/CitiesList.vue'
-  import HelloWorld from './components/HelloWorld.vue'
+  import NavBar from './components/NavBar.vue'
 
   export default {
     components: {
-      CitiesList,
-      HelloWorld
+      NavBar
     }
   }
 </script>
 
 <template>
-  <HelloWorld msg="Hello World!" />
-  <CitiesList />
+  <NavBar />
+  <main>
+    <div class="container grid-container">
+      <img src="../assets/img/skarmkalibrering_02.jpg" />
+      <p>Testbild, 2022</p>
+      <img src="../assets/img/skarmkalibrering_02.jpg" />
+      <img src="../assets/img/skarmkalibrering_02.jpg" />
+      <img src="../assets/img/skarmkalibrering_02.jpg" />
+    </div>
+  </main>
 </template>
+
+<style lang="scss">
+  @import '../assets/main.scss';
+
+  .grid-container {
+    display: flex;
+    flex-direction: column;
+
+    img {
+      margin-top: 15px;
+      margin-bottom: 10px;
+      width: 100%;
+      height: 100%;
+    }
+
+    p {
+      font-size: 12px;
+    }
+  }
+</style>
