b940ce98cd2f199fefb6f4ab60f815a766c7ce7c
Moa Englander <moaenglander@gmail.com>
2023-05-04 16:06:34 +0200

    nytt

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,43 @@
 
 <template>
   <NavBar />
+
   <main>
     <div class="container grid-container">
-      <img src="../assets/img/skarmkalibrering_02.jpg" />
-      <p>Testbild, 2022</p>
-      <img src="../assets/img/skarmkalibrering_02.jpg" />
-      <img src="../assets/img/skarmkalibrering_02.jpg" />
-      <img src="../assets/img/skarmkalibrering_02.jpg" />
+      <img src="../assets/img/naturally.jpg" />
+      <!--  <p>Naturally, 2022</p> -->
+      <img src="../assets/img/curtain.jpg" />
+      <img src="../assets/img/desk.jpg" />
     </div>
   </main>
+
+  <footer class="footer">
+    <div class="footer-container">
+      <div class="row">
+        <ul class="social-icons">
+          <li>
+            <a href="https://instagram.com/mathias3dstrand"
+              ><i class="fab fa-instagram"></i
+            ></a>
+            <a
+              href="https://www.linkedin.com/in/mathias-w%C3%A5gestrand-317732261/"
+              ><i class="fab fa-linkedin"></i
+            ></a>
+
+            <a href="mailto:moaenglander@gmail.com">
+              <i class="far fa-envelope"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="copy">
+      <span
+        ><i class="fa-regular fa-copyright"></i> 2023 Moa Englander, all rights
+        reserved.</span
+      >
+    </div>
+  </footer>
 </template>
 
 <style lang="scss">
@@ -27,16 +55,54 @@
   .grid-container {
     display: flex;
     flex-direction: column;
+    margin-bottom: 25px;
 
     img {
       margin-top: 15px;
-      margin-bottom: 10px;
       width: 100%;
       height: 100%;
     }
 
     p {
       font-size: 12px;
+    }
+  }
+
+  .footer {
+    .footer-container {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 40px;
+      margin-top: 10px;
+      .social-icons {
+        a {
+          text-align: center;
+          padding: 10px;
+          font-size: 23px;
+        }
+      }
+    }
+    .copy {
+      margin-top: 35px;
+      margin-bottom: 10px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 5px;
+      font-size: 12px;
+      color: gray;
+    }
+  }
+  @media screen and (min-width: 768px) {
+    .grid-container {
+      flex-direction: row;
+      gap: 20px;
+      align-items: center;
+      justify-content: center;
+      img {
+        width: 400px;
+      }
     }
   }
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,178 +1,122 @@
-<script></script>
+<script>
+  export default {
+    data() {
+      return {
+        menuActive: false
+      }
+    },
+    methods: {
+      toggleMenu() {
+        this.menuActive = !this.menuActive
+      }
+    }
+  }
+</script>
+
 <template>
-  <nav>
-    <div class="navbar">
-      <div class="container nav-container">
-        <!-- <div class="logo"></div> -->
-        <input class="checkbox" type="checkbox" name="" id="" />
-        <div class="hamburger-lines">
-          <span class="line line1"></span>
-          <span class="line line2"></span>
-          <span class="line line3"></span>
-        </div>
-        <div class="menu-items">
-          <li><a href="#">about</a></li>
-          <li><a href="#">about</a></li>
-          <li><a href="#">about</a></li>
-        </div>
-      </div>
-    </div>
+  <nav class="navbar">
+    <a href="#" class="navbar-logo">M━W</a>
+    <button class="navbar-toggle" @click="toggleMenu">
+      <span class="navbar-toggle-icon"></span>
+      <span class="navbar-toggle-icon"></span>
+      <span class="navbar-toggle-icon"></span>
+    </button>
+    <ul :class="['navbar-menu', menuActive ? 'navbar-menu-active' : '']">
+      <li><a href="#">About</a></li>
+      <li><a href="#">Projects</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
   </nav>
 </template>
+
 <style lang="scss">
-  .container {
-    max-width: 1050px;
-    width: 90%;
-    margin: auto;
-  }
-
   .navbar {
-    width: 100%;
-    box-shadow: 0 1px 4px rgb(146 161 176 / 15%);
-  }
-
-  .nav-container {
     display: flex;
-    justify-content: space-between;
     align-items: center;
-    height: 62px;
-  }
+    justify-content: space-between;
+    padding: 1rem;
 
-  .navbar {
-    .menu-items {
-      background-color: #fff;
-      display: flex;
+    .navbar-logo {
+      font-size: 2rem;
+      font-weight: bold;
+      color: #333;
     }
 
-    .nav-container {
+    .navbar-toggle {
+      background: none;
+      border: none;
+      cursor: pointer;
+      padding: 0;
+      margin: 0;
+    }
+
+    .navbar-toggle-icon {
       display: block;
-      position: relative;
-      height: 60px;
-
-      a {
-        color: #0e2431;
-        font-weight: 500;
-        font-size: 1.2rem;
-        padding: 0.7rem;
-      }
-
-      .checkbox {
-        position: absolute;
-        display: block;
-        height: 32px;
-        width: 32px;
-        top: 20px;
-        left: 300px;
-        z-index: 5;
-        opacity: 0;
-      }
-      input[type='checkbox']:checked ~ .menu-items {
-        transform: translateX(0);
-      }
-
-      input[type='checkbox']:checked ~ .hamburger-lines {
-        .line1 {
-          transform: rotate(45deg);
-        }
-
-        .line2 {
-          transform: scaleY(0);
-        }
-
-        .line3 {
-          transform: rotate(-45deg);
-        }
-      }
-
-      .hamburger-lines {
-        display: block;
-        height: 21px;
-        width: 26px;
-        position: absolute;
-        top: 17px;
-        left: 300px;
-        z-index: 2;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-
-        .line {
-          display: block;
-          height: 2px;
-          width: 100%;
-          border-radius: 10px;
-          background: #0e2431;
-        }
-
-        .line1 {
-          transform-origin: 0% 0%;
-          transition: transform 0.4 ease-in-out;
-        }
-
-        .line2 {
-          transition: transform 0.2 ease-in-out;
-        }
-
-        .line3 {
-          transform-origin: 0% 100%;
-          transition: transform 0.4 ease-in-out;
-        }
-      }
+      width: 1.5rem;
+      height: 0.15rem;
+      margin: 5px;
+      background-color: #333;
+      border-radius: 0.25rem;
+      transition: transform 0.2s ease;
     }
 
-    .menu-items {
-      padding-top: 120px;
-      box-shadow: inset 0 0 2000px rgba(255, 255, 255, 0.5);
-      height: 100vh;
+    .navbar-menu {
+      display: none;
+      position: absolute;
+      top: 4.5rem;
+      right: 0;
+      height: 100%;
       width: 100%;
-      transform: translate(-150%);
-      display: flex;
-      flex-direction: column;
-      margin-left: -40px;
-      padding-left: 50px;
-      transition: transform 0.5s ease-in-out;
-      text-align: center;
+      background-color: #fff;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
 
       li {
-        margin-bottom: 1.2rem;
-        font-size: 1.5rem;
-        font-weight: 500;
+        margin-top: 70px;
+        padding: 5px;
+        font-size: 25px;
+        border-bottom: 1px solid #ddd;
+        border: none;
       }
     }
   }
 
-  @media (min-width: 1024px) {
+  /* desktop styles */
+  @media screen and (min-width: 768px) {
     .navbar {
-      .menu-items {
-        display: flex;
-        flex-direction: row;
-        padding: 15px;
-        height: auto;
-        width: auto;
-        transform: none;
-        box-shadow: none;
-        margin-left: 0;
-        padding-left: 0;
-
-        li {
-          margin-bottom: 0;
-          font-size: 1.2rem;
-        }
-      }
-      .nav-container {
-        height: auto;
-        display: flex;
-        justify-content: flex-end;
-        padding-right: 50px;
-
-        a {
-          padding: 1rem;
-        }
-      }
-
-      .nav-container > .hamburger-lines {
+      justify-content: flex-start;
+      .navbar-toggle {
         display: none;
       }
+
+      .navbar-menu {
+        display: flex;
+        margin-left: auto;
+        position: static;
+        width: auto;
+        background-color: transparent;
+        box-shadow: none;
+      }
+
+      .navbar-menu li {
+        margin-top: 0;
+        font-size: 15px;
+        border: none;
+        margin-right: 3rem;
+      }
     }
+    .navbar-logo {
+      margin-left: 3rem;
+    }
+  }
+
+  /* JavaScript for toggling the navbar menu */
+  .navbar-menu-active {
+    display: flex !important;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .navbar-toggle.active .navbar-toggle-icon {
+    transform: rotate(90deg);
   }
 </style>
